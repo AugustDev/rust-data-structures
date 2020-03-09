ca742ebd5e08a6d9effe99414eaa01f0cecb1634
Augustinas Malinauskas <amirascareless@gmail.com>
2020-03-09 11:10:36 +0000

    imorts

---
+++ src/data_structures.rs
@@ -0,0 +1,9 @@
+pub mod linked_list {
+    pub struct LinkedList {
+        pub val: i32,
+    }
+}
+
+pub fn testshit() {
+    let x = linked_list::LinkedList { val: 10 };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn hi() {}
+mod data_structures;
+
+use data_structures::linked_list::LinkedList;
 
 fn main() {
     println!("Hello, world!");
+    let x = LinkedList { val: 3 };
+    println!("{}", x.val);
 }
